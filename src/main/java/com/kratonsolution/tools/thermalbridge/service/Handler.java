c677a077773ab50ba4400e632a3e8cf8a8e41c2d
agungperdana <agung.dodi.perdana@gmail.com>
2020-01-26 15:29:24 +0700

    add handler code

--- src/main/java/com/kratonsolution/tools/thermalbridge/service/Handler.java
+++ src/main/java/com/kratonsolution/tools/thermalbridge/service/Handler.java
@@ -6,6 +6,7 @@ package com.kratonsolution.tools.thermalbridge.service;
 import io.undertow.io.Receiver.FullBytesCallback;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import javafx.scene.web.WebEngine;
 
 /**
  * @author agung
@@ -23,6 +24,23 @@ public class Handler implements HttpHandler {
 
 		@Override
 		public void handle(HttpServerExchange exchange, byte[] message) {
+			
+			WebEngine engine = new WebEngine();
+			engine.loadContent(new String(message));
+			
+			if(exchange.getRequestHeaders().get("PRINTER_TYPE").contains("THERMAL")) {
+				printThermal(message);
+			}
+			else {
+				printNonThermal(message);
+			}
+		}
+		
+		public void printThermal(byte[] content) {
+			
+		}
+		
+		public void printNonThermal(byte[] content) {
 			
 		}
 	}
